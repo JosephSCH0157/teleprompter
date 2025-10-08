fb45c46115b3f3c51b0ce27362edceee46412922
JosephSCH0157 <Jschramer@gmail.com>
2025-10-07 21:42:35 -0400

    feat: add checks for teleprompter_pro.html existence and improve startup feedback

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,13 @@
 @echo off
 cd /d "%~dp0"
+echo Working dir: %CD%
+if exist teleprompter_pro.html (
+  echo Found: %CD%\teleprompter_pro.html
+) else (
+  echo ERROR: teleprompter_pro.html not found in %CD%
+  pause
+  exit /b 1
+)
 where npx >nul 2>nul && (
   npx http-server -a 127.0.0.1 -p 5180 -c-1
   goto :eof
