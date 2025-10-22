12927e0cbd0b76922d11eff5382d3e86c1ae2a02
JosephSCH0157 <Jschramer@gmail.com>
2025-10-22 12:18:26 -0400

    feat: add global type definitions for Window interface and environment variables

---
+++ src/globals.d.ts
@@ -0,0 +1,15 @@
+interface Window {
+  __TP_DEV?: boolean;
+  __TP_CALM?: boolean;
+  __tpScrollWrite?: (top: number) => void;
+  __tpMatcher?: { matchBatch?: Function };
+  HUD?: any;
+  __tpObsBridge?: any;
+  _initCore?: Function;
+  __lastScrollTarget?: any;
+}
+
+declare const __TP_DEV: boolean | undefined;
+declare const __TP_CALM: boolean | undefined;
+
+export {};
