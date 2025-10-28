60d106d52408494f05dcbd04f8549542200d2bcc
JosephSCH0157 <Jschramer@gmail.com>
2025-10-28 17:01:30 -0400

    feat: enhance inline formatting in renderScript function

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ import { installScrollRouter } from './features/scroll-router.js';
 import { initScroll } from './features/scroll.js';
 import { installSpeech } from './features/speech-loader.js';
 import { initTelemetry } from './features/telemetry.js';
+// Ensure inline formatter is present (provides window.formatInlineMarkup)
+import '../ui/format.js';
 import { initToasts } from './features/toasts.js';
 import * as UI from './ui/dom.js';
 
